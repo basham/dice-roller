9711cc6951f8156aa428b362d18638d1d06ecbcc
Chris Basham <csbasham@iu.edu>
2019-11-07 11:34:54 -0500

    Add icons and pass PWA audit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
 if (workbox) {
+  workbox.precaching.precacheAndRoute([
+    '/index.html'
+  ])
   workbox.routing.registerRoute(
     /\.(?:js|css|svg)$/,
     new workbox.strategies.StaleWhileRevalidate(),
   )
-  console.log(`Yay! Workbox is loaded 🎉`)
 }
