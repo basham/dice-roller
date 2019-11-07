0cdd880872b6cbbbefa81bd2d516562b15ccff20
Chris Basham <csbasham@iu.edu>
2019-11-07 14:25:37 -0500

    Rework caching index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,12 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
 if (workbox) {
-  workbox.precaching.precacheAndRoute([
-    '/index.html'
-  ])
   workbox.routing.registerRoute(
-    /\.(?:js|css|svg)$/,
+    '/',
+    new workbox.strategies.StaleWhileRevalidate(),
+  )
+  workbox.routing.registerRoute(
+    /\.(?:js|css|svg|html)$/,
     new workbox.strategies.StaleWhileRevalidate(),
   )
 }
