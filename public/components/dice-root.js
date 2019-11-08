459ad25749f5176cf36a0061030cd7b5a64aff9a
Chris Basham <chris@bash.am>
2019-11-08 15:08:26 -0500

    Fix :active styles in iOS Safari

--- public/components/dice-root.js
+++ public/components/dice-root.js
@@ -3,6 +3,10 @@ import styles from './dice-root.css'
 
 adoptStyles(styles)
 
+// Enable :active styles in iOS Safari.
+// https://css-tricks.com/snippets/css/remove-gray-highlight-when-tapping-links-in-mobile-safari/
+document.addEventListener('touchstart', () => {}, true)
+
 define('dice-root', (el) => {
   render(el, renderRoot)
 })
